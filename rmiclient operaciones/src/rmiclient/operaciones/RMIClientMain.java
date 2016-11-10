08a2fc2320b8890864a1f62c1128dae174673c34
Axel <Axel@Cuzo>
2016-11-10 07:40:56 -0600

    Todas las clases

---
+++ rmiclient operaciones/src/rmiclient/operaciones/RMIClientMain.java
@@ -0,0 +1,34 @@
+package rmiclient.operaciones;
+
+
+
+
+
+public class RMIClientMain 
+{
+     public static void main(String[] args) 
+    {
+        try {
+            RmiCommunicationClient wsc = new RmiCommunicationClient();
+
+            int suma = wsc.getSuma(1, 1);
+            int resta = wsc.getResta(1, 1);
+            int multiplicacion = wsc.getMultiplicacion(1, 1);
+            int division = wsc.getDivision(5, 0);
+            int modulo = wsc.getModulo(1, 1);
+            int potencia = wsc.getPotencia(1, 1);
+            
+            
+             System.out.println(" resultado de la suma por el servidor: " + suma);
+             System.out.println(" resultado de la resta por el servidor: " + resta);
+             System.out.println(" resultado de la mult por el servidor: " + multiplicacion);
+             System.out.println(" resultado de la divi por el servidor: " + division);
+             System.out.println(" resultado de el mod por el servidor: " + modulo);
+             System.out.println(" resultado de la potencia por el servidor: " + potencia);
+             
+        } catch (Exception ex) {
+            ex.printStackTrace();
+             System.err.println("Connection error the server is not responding.");
+        }
+    }
+}
---
+++ rmiclient operaciones/src/rmiclient/operaciones/RmiCommunicationClient.java
@@ -0,0 +1,49 @@
+package rmiclient.operaciones;
+
+/**
+ *
+ * @author Emmanuel Oropeza
+ */
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RmiCommunicationClient {
+
+    //Operational Classes
+
+    private final Registry _registry;
+    private final InterfaceRmiCommunication _interfaceRmiCommunication;
+    private final int RMIPort = 1234;
+
+    //Constructors
+    public RmiCommunicationClient() throws RemoteException, NotBoundException, IOException {
+        System.setProperty("java.security.policy", "java.policy");
+        System.setSecurityManager(new RMISecurityManager());
+        this._registry = LocateRegistry.getRegistry("192.168.9.121",1234);
+        this._interfaceRmiCommunication = (InterfaceRmiCommunication) _registry.lookup("rmiServer");
+    }
+
+    public Integer getSuma(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getSuma(num1, num2);
+    }
+    public Integer getResta(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getResta(num1, num2);
+    }
+    public Integer getMultiplicacion(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getMultiplicacion(num1, num2);
+    }
+    public Integer getDivision(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getDivision(num1, num2);
+    }
+    public Integer getModulo(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getModulo(num1, num2);
+    }
+    public Integer getPotencia(int num1, int num2) throws RemoteException {
+        return _interfaceRmiCommunication.getPotencia(num1, num2);
+    }
+    
+}
